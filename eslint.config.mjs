e3cbf712161f60ad589c5bf5dc8d78cb200cff1a
dee-diaz <diazthediablo@yahoo.com>
2026-03-15 09:06:57 -0300

    chore(deps): add typescript linter and update eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,30 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['webpack.*.js']),
+  globalIgnores(['webpack.*.js', 'dist', 'node_modules']),
+
   {
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
     extends: ['js/recommended'],
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      globals: globals.browser,
+    },
   },
+
+  ...tseslint.configs.recommended,
+
   {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+    },
     rules: {
-      'no-unused-vars': 'warn',
-      'no-undef': 'error',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
     },
   },
 ]);
