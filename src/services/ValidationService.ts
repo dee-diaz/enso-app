198b034a3cf3b3c4f01201617f05ecc23a4af293
dee-diaz <diazthediablo@yahoo.com>
2026-03-18 14:12:30 -0300

    refactor(services): migrate ValidationService to TypeScript

--- src/services/ValidationService.ts
+++ src/services/ValidationService.ts
@@ -1,10 +1,12 @@
 // Task/List validation rules
 class ValidationService {
-  static validateInput(input) {
+  static validateInput(input: string): boolean {
     const minLength = 1;
     if (input.trim().length < minLength) {
       console.warn('Title should be at least 1 character');
-      document.querySelector("#task-title").focus();
+      const titleInput =
+        document.querySelector<HTMLInputElement>('#task-title');
+      titleInput!.focus();
       return false;
     } else {
       return true;
